a9437057e129bf8abe6411794159c534d6aa6e59
Prasun Kumar <prasun.code@gmail.com>
2021-03-09 11:35:23 +0530

    Added new urls for coupons

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,4 +1,5 @@
 const BASE_URL = 'http://ec2-3-23-96-26.us-east-2.compute.amazonaws.com:8000/api/v1/auth';
+const API_URL = "http://ec2-3-23-96-26.us-east-2.compute.amazonaws.com:8000/api/v1";
 
 export const LOGIN_URL = BASE_URL + '/login/';
 export const LOGOUT_URL = BASE_URL + '/logout/';
@@ -8,3 +9,5 @@ export const PASSWORD_RESET_CONFIRM = BASE_URL + '/password/reset/confirm';
 export const REGISTER = BASE_URL + '/register/';
 export const REGISTER_VERIFY_EMAIL = BASE_URL + '/register/verify-email/';
 export const USER = BASE_URL + '/user/';
+export const REQUEST_COUPON = API_URL + '/coupon/buy/request/';
+export const CONFIRM_COUPON_PAYMENT = API_URL + '/coupon/buy/confirm/';
